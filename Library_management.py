4feb51b01cbec9e103e7abfa1268c475277f436a
Juan Darquea <juan.darquea.a@gmail.com>
2025-07-16 02:58:40 -0500

    V1.1 Library Management System
    Tiny basic management system for librarys, my goal is to learn and practice.
    
    Current functions:
    1. Show book information
    2. Borrow book (no choice on book)
    3. Return book (no choice on book)

--- Library_management.py
+++ Library_management.py
@@ -14,5 +14,22 @@ class Book:
                                                                  # Print book details
         print(f"Title: {self.title}", f"\nAuthor: {self.author}", f"\nYear: {self.year}", f"\nISBN: {self.isbn}", f"\nStatus: {status}")
 
+    def borrow(self):                               # Method to borrow a book
+        """Borrow the book if it is available."""
+        if self.is_borrowed:                        # Check if the book is already borrowed
+            print("El libro ya está prestado.")
+        else:                                       # If the book is available
+            self.is_borrowed = True                 # Set book status to borrowed
+            print("Libro prestado correctamente.")
+
+    def return_book(self):                           # Method to return a borrowed book
+        """Return the borrowed book."""
+        if not self.is_borrowed:                     # Check if the book is not borrowed
+            print("El libro ya está disponible en la biblioteca.")
+        else:                                        # If the book is borrowed
+            self.is_borrowed = False                 # Set book status to available
+            print("Libro devuelto correctamente.")
+
 book1 = Book("1984", "George Orwell", 1949, "1234567890") # First book instance
 book1.show_book_info()                                    # Display book information
+
